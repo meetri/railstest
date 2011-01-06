3e5d99fd6f9c7e80ab499d1bb4005118cec16ae4
Demetrius Bell <one@ptechstudios.com>
2011-01-06 05:41:15 +0000

    actionmailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110105175319) do
+ActiveRecord::Schema.define(:version => 20110105175320) do
 
   create_table "badges", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20110105175319) do
     t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
