3abb30c0ccadc53e02fefdef48d8b79be432dda6
Demetrius Bell <meetri@gmail.com>
2011-01-05 16:13:55 -0800

    cancan integration 4

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,13 @@ class Ability
   include CanCan::Ability
   
   def initialize(user)
-    user ||= User.new # guest user
+    #user ||= User.new # guest user
+    
     
     if user.admin?
       can :manage, :all
-    else
-      can :read, :all
+    #else user != nil
+    #  can :read, :all
       
     end
   end
